a41f19793e0d63329805d2c6d698cb5743d8fb96
tabib-e-alahi <etabib.alahi@gmail.com>
2023-11-26 11:00:44 +0600

    tailwind and material tailwind installed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@material-tailwind/react";
 
 
 const Home = () => {
     return (
         <div>
             <h1>This is Home</h1>
+            <Button>Button</Button>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/Routes';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { ThemeProvider } from "@material-tailwind/react";
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
